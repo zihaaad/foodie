41b901ab0ae0cdcac78b6277fc0dc1e54237e1ca
Zihadul Islam <zihadislam2429@gmail.com>
2024-05-05 05:46:40 +0600

    Place Order Page Created

--- client/src/pages/PlaceOrder/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,7 +1,57 @@
+import {useContext} from "react";
 import "./PlaceOrder.css";
+import {StoreContext} from "../../context/StoreContext";
 
 const PlaceOrder = () => {
-  return <></>;
+  const {getTotalCartAmmount} = useContext(StoreContext);
+  return (
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <input type="=email" placeholder="Email Address" />
+        <input type="text" placeholder="Street" />
+        <div className="multi-fields">
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="State" />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder="Zip code" />
+          <input type="text" placeholder="Country" />
+        </div>
+        <input type="text" placeholder="Phone" />
+      </div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmmount() > 0 ? 2 : 0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>
+                ${getTotalCartAmmount() > 0 ? getTotalCartAmmount() + 2 : 0}
+              </b>
+            </div>
+            <button onClick={() => navigate("/order")}>
+              PROCEED TO PAYMENT
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default PlaceOrder;
